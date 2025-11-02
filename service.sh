62c25bde31240c1b4fe065225ce1c8369fb02337
Kristal <117694943+AkasTKzume69@users.noreply.github.com>
2025-11-02 18:06:57 +0800

    Create service.sh

---
+++ service.sh
@@ -0,0 +1,10 @@
+#!/system/bin/sh
+#Switch to vulkanskia renderthread backend pipeline delayed cleanup
+# Wait a bit to let system fully boot
+sleep 30
+# Kill user apps except system ones safely
+for a in $(pm list packages -3 | cut -f2 -d:); do
+    am force-stop "$a" >/dev/null 2>&1 &
+done
+#Log completion
+echo "[VulkanRendererSwitch] Forced user apps restart to apply Vulkan" >> /data/local/tmp/vulkan_renderer.log

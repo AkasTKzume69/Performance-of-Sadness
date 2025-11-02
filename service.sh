5ff9c01364e45a0946e7dd3e2a96740cc86a75d7
Kristal <117694943+AkasTKzume69@users.noreply.github.com>
2025-11-02 18:07:56 +0800

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,8 +1,8 @@
 #!/system/bin/sh
-#Switch to vulkanskia renderthread backend pipeline delayed cleanup
-# Wait a bit to let system fully boot
+#####Switch to vulkanskia renderthread backend pipeline delayed cleanup#####
+#Wait a bit to let system fully boot
 sleep 30
-# Kill user apps except system ones safely
+#Kill user apps except system ones safely
 for a in $(pm list packages -3 | cut -f2 -d:); do
     am force-stop "$a" >/dev/null 2>&1 &
 done

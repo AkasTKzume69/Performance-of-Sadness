950a77b4c32f5444b4e8276285e8a4dfa62e2dd5
Kristal <117694943+AkasTKzume69@users.noreply.github.com>
2025-11-03 19:27:04 +0800

    Update post-fs-data.sh

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -1,6 +1,4 @@
 #!/system/bin/sh
-#####Switch to vulkan renderthread backend pipeline#####
 #Set Vulkan renderer early before UI starts
 setprop debug.hwui.renderer skiavk
-#Log
-echo "[VulkanRendererSwitch] Renderer set to Vulkan" > /data/local/tmp/vulkan_renderer.log
+#End of file

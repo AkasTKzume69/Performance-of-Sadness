3e7f19f668be78fd5c7404b203e78004ec30ed6a
Kristal <117694943+AkasTKzume69@users.noreply.github.com>
2025-11-03 19:39:52 +0800

    Update service.sh

--- service.sh
+++ service.sh
@@ -1,9 +1,9 @@
 #!/system/bin/sh
 #Wait a bit to let system fully boot
-sleep 25
+sleep 30
 #Kill user apps except system ones safely
 for a in $(pm list packages -3 | cut -f2 -d:); do
     am force-stop "$a" >/dev/null 2>&1 &
 done
 #Log completion
-echo "[VulkanRendererSwitch] Forced user apps restart to apply Vulkan" >> /storage/emulated/0/Performance-of-Sadness.log
+echo "[VulkanRendererSwitch] Forced user apps restart to apply Vulkan" >> /sdcard/Performance-of-Sadness.log

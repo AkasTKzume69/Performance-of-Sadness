e0be768398fa8b4ae30d4df6fa6866c75693e965
Kristal <117694943+AkasTKzume69@users.noreply.github.com>
2025-11-04 10:15:49 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,12 +1,4 @@
 set_permissions() {
-  set_perm  $MODPATH/system/vendor/etc/thermal-arvr.conf       0       0       0644
-  set_perm  $MODPATH/system/vendor/etc/thermal-engine.conf       0       0       0644
-  set_perm  $MODPATH/system/vendor/etc/thermal-extreme.conf       0       0       0644
-  set_perm  $MODPATH/system/vendor/etc/thermal-high.conf       0       0       0644
-  set_perm  $MODPATH/system/vendor/etc/thermal-map.conf       0       0       0644
-  set_perm  $MODPATH/system/vendor/etc/thermal-nolimits.conf       0       0       0644
-  set_perm  $MODPATH/system/vendor/etc/thermal-normal.conf       0       0       0644
-  set_perm  $MODPATH/system/vendor/etc/thermal-tgame.conf       0       0       0644
   set_perm  $MODPATH/system/vendor/bin/perf_profile.sh       0       0       0755
   set_perm  $MODPATH/system/vendor/bin/restore_perf_profile.sh       0       0       0755
 }

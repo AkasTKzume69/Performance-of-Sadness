f3d3b407871a24bcfd55ca9fcf4dd1cd1714b587
Kristal <117694943+AkasTKzume69@users.noreply.github.com>
2025-11-17 16:06:24 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,8 +1,9 @@
 set_permissions() {
   set_perm  $MODPATH/system/vendor/etc/msm_irqbalance.conf 0 0 0644
   set_perm  $MODPATH/system/vendor/etc/powerhint.json 0 0 0644
-  set_perm  $MODPATH/system/vendor/bin/perf_profile.sh 0 0 0777
-  set_perm  $MODPATH/system/vendor/bin/restore_perf_profile.sh 0 0 0777
+  set_perm  $MODPATH/system/vendor/etc/game-list.pos 0 0 0644
+  set_perm  $MODPATH/system/vendor/bin/perf_profile.pos 0 0 0777
+  set_perm  $MODPATH/system/vendor/bin/perf_profile_restore.pos 0 0 0777
 }
 
 SKIPUNZIP=1
